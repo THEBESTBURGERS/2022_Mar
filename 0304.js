0ea5cb03f256b4459aa847339a9ed2fb2e47aa1f
chestnut1044 <wbk1620@gmail.com>
2022-03-08 15:35:31 +0900

    Learn : Function

---
+++ 0304.js
@@ -0,0 +1,13 @@
+// (function () {
+//     var a = 3;
+//     var b = 5;
+//     return a*b;
+// } ());
+
+
+// (function foo() {
+//     var a = 3;
+//     var b = 5;
+//     return a*b;
+// } foo());
+
