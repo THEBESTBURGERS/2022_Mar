ea4e09a2a909302a66df4f03efa5342e8f88bac8
chestnut1044 <wbk1620@gmail.com>
2022-03-11 00:27:30 +0900

    Learn : Scope

--- 0309.js
+++ 0309.js
@@ -1,14 +1,31 @@
-var x = 1;
+// var x = 1;
 
+// function foo() {
+//   var x = 10;
+//   bar();
+// }
+
+// function bar() {
+//   console.log(x);
+// }
+
+// console.log('실행')
+// foo(); // ?
+// bar(); // ?
+
+
+// 전역 함수
 function foo() {
-  var x = 10;
-  bar();
+  console.log('global function foo');
 }
 
 function bar() {
-  console.log(x);
+  // 중첩 함수
+  function foo() {
+    console.log('local function foo');
+  }
+
+  foo(); // ①
 }
 
-console.log('실행')
-foo(); // ?
-bar(); // ?
+bar();
